e51c36a6c6a945ebb2fa999a530c5cf95c0c09d7
Diego Castilho <163672892+DigoCast@users.noreply.github.com>
2026-03-26 20:39:33 -0300

    Feat/callscreen (#5)
    * feat: create tickets screen and newTicket screen
    
    * feat: add stack logic for newTicket screen
    
    * update: remove sector fild for tickets

--- frontend/src/app/(client)/_layout.tsx
+++ frontend/src/app/(client)/_layout.tsx
@@ -1,45 +1,10 @@
-import { Tabs } from 'expo-router';
-import { FontAwesome } from '@expo/vector-icons'; 
+import { Stack } from 'expo-router';
 
-export default function ClientLayout() {
+export default function ClientStackLayout() {
   return (
-    <Tabs 
-      screenOptions={{ 
-        headerShown: false, 
-        tabBarActiveTintColor: '#2563eb', 
-        tabBarInactiveTintColor: '#94a3b8', 
-        tabBarStyle: {
-          paddingBottom: 5,
-          paddingTop: 5,
-          height: 60,
-        }
-      }}
-    >
-      
-      <Tabs.Screen 
-        name="clientHome" 
-        options={{
-          title: 'Início',
-          tabBarIcon: ({ color }) => <FontAwesome name="home" size={24} color={color} />
-        }}
-      />
-
-      <Tabs.Screen 
-        name="tickets" 
-        options={{
-          title: 'Chamados',
-          tabBarIcon: ({ color }) => <FontAwesome name="ticket" size={24} color={color} />
-        }}
-      />
-      
-      <Tabs.Screen 
-        name="profile" 
-        options={{
-          title: 'Perfil',
-          tabBarIcon: ({ color }) => <FontAwesome name="user" size={24} color={color} />
-        }}
-      />
-
-    </Tabs>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="newTicket" />
+    </Stack>
   );
 }
