6b0fa2fa348145d6a111043b3eb9b35c84cb31a6
YgorPereira <ygor.pereira@autaza.com>
2026-04-03 17:51:33 -0300

    refactor: add a enviroment checker to define the toke storage

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react';
-import * as SecureStore from 'expo-secure-store';
+import { storage } from '@/utils/storage';
 import api from '@/services/api';
 import { jwtDecode } from "jwt-decode"
 
@@ -27,8 +27,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode}) => {
 
     useEffect(() => {
         async function loadStorageData() {
-            const storedToken = await SecureStore.getItemAsync('prodesk_token')
-            const storedUser = await SecureStore.getItemAsync('prodesk_user')
+            const storedToken = await storage.getItem('prodesk_token')
+            const storedUser = await storage.getItem('prodesk_user')
 
             if (storedToken && storedUser) {
                 api.defaults.headers.common['Authorization'] = `Bearer ${storedToken}`
@@ -55,8 +55,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode}) => {
                 role: decoded.role.toLowerCase() as UserRole
             }
 
-            await SecureStore.setItemAsync('prodesk_token', token)
-            await SecureStore.setItemAsync('prodesk_user', JSON.stringify(userData))
+            await storage.setItem('prodesk_token', token)
+            await storage.setItem('prodesk_user', JSON.stringify(userData))
 
             api.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
@@ -72,8 +72,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode}) => {
     }
 
     const signOut = async () => {
-        await SecureStore.deleteItemAsync('prodesk_token');
-        await SecureStore.deleteItemAsync('prodesk_user');
+        await storage.removeItem('prodesk_token');
+        await storage.removeItem('prodesk_user');
         setUser(null)
     }
 
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,12 +1,12 @@
+import { storage } from '@/utils/storage'
 import axios from 'axios'
-import * as SecureStore from 'expo-secure-store';
 
 const api = axios.create({
     baseURL: 'http://10.0.2.2:3000/ProDeskApi'
 })
 
 api.interceptors.request.use(async (config) => {
-    const token = await SecureStore.getItemAsync('prodesk_token') 
+    const token = await storage.getItem('prodesk_token') 
 
     if (token) {
         config.headers.Authorization =  `Bearer ${token}`
